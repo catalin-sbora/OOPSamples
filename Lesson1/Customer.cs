fa670ec81eb7f27ac1ef7583594d3db69734fc0f
catalin <catalin.sbora@gmail.com>
2019-04-11 13:39:27 +0300

    - add first project

---
+++ Lesson1/Customer.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson1
+{
+    public class Customer
+    {
+        public int Id{ get; set;}
+        public string Name{get; set;}
+        public string PhoneNumber{ get; set;}
+    }
+}
---
+++ Lesson1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataRepository repository = new DataRepository();            
+            Order newOrder = new Order(repository.ProductsStock);
+            try
+            {
+                var selectedProd = repository.Products
+                            .Where(product => product.Id == 1)
+                            .Single();
+
+                newOrder.AddItem(selectedProd, 10);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("The item that you are trying to add has an invalid value." + e.Message );    
+            }
+            
+        }
+    }
+}
